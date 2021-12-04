9e4559bd024eac86542796c7f811d24aad3d093f
sfeucht <sheridan.feucht@gmail.com>
2021-12-04 14:05:03 -0500

    refactor: copy over server branch, now all backend is on this branch.

---
+++ src/main/java/com/cs32/lettersmod/Address.java
@@ -0,0 +1,42 @@
+package com.cs32.server;
+
+/**
+ * A class that stores address info.
+ */
+public class Address {
+
+  private String address;
+  private final String trueKey;
+  private String active;
+
+  /**
+   * Constructor for an Address object.
+   * @param address - The displayed address.
+   * @param trueKey - The real ID.
+   * @param active - True if the address is able to receive mail, else false.
+   */
+  public Address(String address, String trueKey, String active) {
+    this.address = address;
+    this.trueKey = trueKey;
+    this.active = active;
+  }
+
+  public String getAddress() {
+    return this.address;
+  }
+
+  public String getTrueKey() {
+    return this.trueKey;
+  }
+
+  public boolean getActive() {
+    if (active.equals("true")) {
+      return true;
+    } else if (active.equals("false")) {
+      return false;
+    } else {
+      throw new RuntimeException("ERROR: There was an issue with the" +
+          "formatting of the active attribute.");
+    }
+  }
+}
