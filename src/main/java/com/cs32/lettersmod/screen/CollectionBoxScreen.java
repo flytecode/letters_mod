301c23478ed1dfa2c6003617bbaad6a5f3b67eea
sfeucht <sheridan.feucht@gmail.com>
2021-12-11 17:07:23 -0500

    revert collection box gui back to tutorial and try again

--- src/main/java/com/cs32/lettersmod/screen/CollectionBoxScreen.java
+++ src/main/java/com/cs32/lettersmod/screen/CollectionBoxScreen.java
@@ -4,63 +4,19 @@ import com.cs32.lettersmod.LettersMod;
 import com.cs32.lettersmod.container.CollectionBoxContainer;
 import com.mojang.blaze3d.matrix.MatrixStack;
 import com.mojang.blaze3d.systems.RenderSystem;
-import net.minecraft.client.gui.DialogTexts;
 import net.minecraft.client.gui.screen.inventory.ContainerScreen;
-import net.minecraft.client.gui.widget.TextFieldWidget;
-import net.minecraft.client.gui.widget.button.Button;
-import net.minecraft.client.multiplayer.ServerData;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.ITextComponent;
-import net.minecraft.util.text.TranslationTextComponent;
 
+import java.util.Collection;
 
-// see AnvilScreen.java for help
 public class CollectionBoxScreen extends ContainerScreen<CollectionBoxContainer> {
   private final ResourceLocation GUI = new ResourceLocation(LettersMod.MOD_ID,
       "textures/gui/collection_box.png");
-  private Button sendButton;
-  private TextFieldWidget textFieldAddress;
-  private String sendAddress;
 
   public CollectionBoxScreen(CollectionBoxContainer screenContainer, PlayerInventory inv, ITextComponent titleIn) {
     super(screenContainer, inv, titleIn);
-  }
-
-  public void tick() {
-    this.textFieldAddress.tick();
-  }
-
-  protected void init() {
-    int i = (this.width - this.xSize) / 2;
-    int j = (this.height - this.ySize) / 2;
-
-    this.minecraft.keyboardListener.enableRepeatEvents(true);
-    this.textFieldAddress = new TextFieldWidget(this.font, i + 62, j + 24, 103, 12, new TranslationTextComponent("addServer.enterName"));
-    this.textFieldAddress.setCanLoseFocus(false);
-    this.textFieldAddress.setTextColor(-1);
-    this.textFieldAddress.setDisabledTextColour(-1);
-    this.textFieldAddress.setEnableBackgroundDrawing(false);
-    this.textFieldAddress.setMaxStringLength(35);
-    this.textFieldAddress.setResponder(this::textFieldResponder);
-    this.children.add(this.textFieldAddress);
-    this.setFocusedDefault(this.textFieldAddress);
-
-//    this.sendButton = this.addButton(new Button(this.width / 2 - 100, this.height / 4 + 96 + 18, 200, 20, new TranslationTextComponent("collectionBox.sendButton"), (input) -> {
-//      this.onButtonServerAddPressed();
-//    }));
-  }
-
-  private void textFieldResponder(String s) { // don't actually use s, just to make compiler happy
-    s = this.textFieldAddress.getText();
-    boolean flag = !s.isEmpty() && s.split(":").length > 0 && s.indexOf(32) == -1;
-    this.sendButton.active = flag && !this.textFieldAddress.getText().isEmpty();
-  }
-
-  private void onButtonServerAddPressed() {
-    this.sendAddress = this.textFieldAddress.getText();
-    //TODO call mailcourier functionality
-    System.out.println(this.sendAddress);
   }
 
   @Override
@@ -68,7 +24,6 @@ public class CollectionBoxScreen extends ContainerScreen<CollectionBoxContainer>
     this.renderBackground(matrixStack);
     super.render(matrixStack, mouseX, mouseY, partialTicks);
     this.renderHoveredTooltip(matrixStack, mouseX, mouseY);
-    this.textFieldAddress.render(matrixStack, mouseX, mouseY, partialTicks);
   }
 
   @Override
@@ -79,7 +34,8 @@ public class CollectionBoxScreen extends ContainerScreen<CollectionBoxContainer>
     int j = this.guiTop;
     this.blit(matrixStack, i, j, 0, 0, this.xSize, this.ySize);
 
-    // if lightning storm
-//    this.blit(matrixStack, i + 82, j + 9, 176, 0, 13, 17);
+    if(container.isLightningStorm()) {
+      this.blit(matrixStack, i + 82, j + 9, 176, 0, 13, 17);
+    }
   }
 }
