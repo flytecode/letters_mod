3f2040c1164eaebb440a3ae3d704668ab1ad0dd9
sfeucht <sheridan.feucht@gmail.com>
2021-12-11 16:30:53 -0500

    feat: a collection box screen that takes input but looks cucked

--- src/main/java/com/cs32/lettersmod/screen/CollectionBoxScreen.java
+++ src/main/java/com/cs32/lettersmod/screen/CollectionBoxScreen.java
@@ -4,20 +4,56 @@ import com.cs32.lettersmod.LettersMod;
 import com.cs32.lettersmod.container.CollectionBoxContainer;
 import com.mojang.blaze3d.matrix.MatrixStack;
 import com.mojang.blaze3d.systems.RenderSystem;
+import net.minecraft.client.gui.DialogTexts;
 import net.minecraft.client.gui.screen.inventory.ContainerScreen;
+import net.minecraft.client.gui.widget.TextFieldWidget;
+import net.minecraft.client.gui.widget.button.Button;
+import net.minecraft.client.multiplayer.ServerData;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.ITextComponent;
-
+import net.minecraft.util.text.TranslationTextComponent;
 
 
 // see AnvilScreen.java for help
 public class CollectionBoxScreen extends ContainerScreen<CollectionBoxContainer> {
   private final ResourceLocation GUI = new ResourceLocation(LettersMod.MOD_ID,
       "textures/gui/collection_box.png");
+  private Button sendButton;
+  private TextFieldWidget textFieldAddress;
+  private String sendAddress;
 
   public CollectionBoxScreen(CollectionBoxContainer screenContainer, PlayerInventory inv, ITextComponent titleIn) {
     super(screenContainer, inv, titleIn);
+  }
+
+  public void tick() {
+    this.textFieldAddress.tick();
+  }
+
+  protected void init() {
+    this.minecraft.keyboardListener.enableRepeatEvents(true);
+    this.textFieldAddress = new TextFieldWidget(this.font, this.width / 2 - 100, 66, 200, 20, new TranslationTextComponent("addServer.enterName"));
+    this.textFieldAddress.setFocused2(true);
+    this.textFieldAddress.setText(this.sendAddress);
+    this.textFieldAddress.setResponder(this::textFieldResponder);
+    this.children.add(this.textFieldAddress);
+
+    this.sendButton = this.addButton(new Button(this.width / 2 - 100, this.height / 4 + 96 + 18, 200, 20, new TranslationTextComponent("collectionBox.sendButton"), (input) -> {
+      this.onButtonServerAddPressed();
+    }));
+  }
+
+  private void textFieldResponder(String s) { // don't actually use s, just to make compiler happy
+    s = this.textFieldAddress.getText();
+    boolean flag = !s.isEmpty() && s.split(":").length > 0 && s.indexOf(32) == -1;
+    this.sendButton.active = flag && !this.textFieldAddress.getText().isEmpty();
+  }
+
+  private void onButtonServerAddPressed() {
+    this.sendAddress = this.textFieldAddress.getText();
+    //TODO call mailcourier functionality
+    System.out.println(this.sendAddress);
   }
 
   @Override
@@ -25,6 +61,7 @@ public class CollectionBoxScreen extends ContainerScreen<CollectionBoxContainer>
     this.renderBackground(matrixStack);
     super.render(matrixStack, mouseX, mouseY, partialTicks);
     this.renderHoveredTooltip(matrixStack, mouseX, mouseY);
+    this.textFieldAddress.render(matrixStack, mouseX, mouseY, partialTicks);
   }
 
   @Override
