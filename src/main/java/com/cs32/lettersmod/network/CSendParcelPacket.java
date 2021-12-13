a643e78f16649c91eebd8b4a15fbefc9af387f36
sfeucht <sheridan.feucht@gmail.com>
2021-12-13 14:45:36 -0500

    refactor: got confused with forge simplechannel vs inethandler

--- src/main/java/com/cs32/lettersmod/network/CSendParcelPacket.java
+++ src/main/java/com/cs32/lettersmod/network/CSendParcelPacket.java
@@ -5,38 +5,43 @@ import net.minecraft.network.IPacket;
 import net.minecraft.network.PacketBuffer;
 import net.minecraft.network.play.IServerPlayNetHandler;
 
-public class CSendParcelPacket implements IPacket<IServerPlayNetHandler> {
-  private String name;
+public class CSendParcelPacket implements IPacket<LettersPacketHandler> {
+  private String address;
+  private String parcelString;
 
   public CSendParcelPacket() {
   }
 
-  public CSendParcelPacket(String name) {
-    this.name = name;
+  public CSendParcelPacket(String address, String parcelString) {
+    this.address = address;
+    this.parcelString = parcelString;
   }
 
   /**
    * Reads the raw packet data from the data stream.
    */
   public void readPacketData(PacketBuffer buf) throws IOException {
-    this.name = buf.readString(32767);
+    this.address = buf.readString(32767);
+    this.parcelString = buf.readString(32767);
   }
 
   /**
    * Writes the raw packet data to the data stream.
    */
   public void writePacketData(PacketBuffer buf) throws IOException {
-    buf.writeString(this.name);
+    buf.writeString(this.address);
+    buf.writeString(this.parcelString);
   }
 
   /**
    * Passes this Packet on to the NetHandler for processing.
    */
-  public void processPacket(IServerPlayNetHandler handler) {
-    handler.processRenameItem(this);
+  public void processPacket(LettersPacketHandler handler) {
+    handler.processSendParcel(this);
   }
 
-  public String getName() {
-    return this.name;
+  public String getAddress() {
+    return this.address;
   }
+  public String getParcelString() { return this.parcelString;}
 }
--- src/main/java/com/cs32/lettersmod/network/LettersPacketHandler.java
+++ src/main/java/com/cs32/lettersmod/network/LettersPacketHandler.java
@@ -1,23 +1,25 @@
 package com.cs32.lettersmod.network;
 
+import net.minecraft.network.INetHandler;
+import net.minecraft.network.NetworkManager;
 import net.minecraft.network.play.IServerPlayNetHandler;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.text.ITextComponent;
 import net.minecraftforge.fml.network.NetworkRegistry;
 import net.minecraftforge.fml.network.simple.SimpleChannel;
 
 
-public class LettersPacketHandler {
-  private static final String PROTOCOL_VERSION = "1";
-  public static final SimpleChannel INSTANCE = NetworkRegistry.newSimpleChannel(
-      new ResourceLocation("lettersmod", "main"),
-      () -> PROTOCOL_VERSION,
-      PROTOCOL_VERSION::equals,
-      PROTOCOL_VERSION::equals
-  ).registerMessage();
-
-
-
-//  public static void handle(MyMessage msg, Supplier<NetworkEvent.Context> ctx) {
+public class LettersPacketHandler implements INetHandler {
+  //this is the forge stuff
+//  private static final String PROTOCOL_VERSION = "1";
+//  public static final SimpleChannel INSTANCE = NetworkRegistry.newSimpleChannel(
+//      new ResourceLocation("lettersmod", "main"),
+//      () -> PROTOCOL_VERSION,
+//      PROTOCOL_VERSION::equals,
+//      PROTOCOL_VERSION::equals
+//  );
+//
+//  public static void processSendParcel(MyMessage msg, Supplier<NetworkEvent.Context> ctx) {
 //    ctx.get().enqueueWork(() -> {
 //      // Work that needs to be thread-safe (most work)
 //      ServerPlayerEntity sender = ctx.get().getSender(); // the client that sent this packet
@@ -25,4 +27,21 @@ public class LettersPacketHandler {
 //    });
 //    ctx.get().setPacketHandled(true);
 //  }
+
+  // this is me trying to copy vanilla stuff
+  public void processSendParcel(CSendParcelPacket p) {
+    System.out.println("parcel: " + p);
+  }
+
+  @Override
+  public void onDisconnect(ITextComponent reason) {
+    System.out.println(reason);
+  }
+
+  @Override
+  public NetworkManager getNetworkManager() {
+    return null;
+  }
+
+
 }
